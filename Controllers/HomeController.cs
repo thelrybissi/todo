6ff4945f229641d475b33b3aa5e823823786ce15
Thelry Bissi <thelry.d.b@hotmail.com>
2022-07-03 16:05:37 -0300

    Generate migrations and db

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Todo.Data;
 using Todo.Model;
--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using Todo.Data;
 
 var builder = WebApplication.CreateBuilder(args);
