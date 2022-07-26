b8852d9aa7612b2192c8d25f6cd5c4592026f439
thelrybissi <thelry.d.b@hotmail.com>
2022-07-25 21:48:33 -0300

    Connection

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -9,7 +9,7 @@ namespace Todo.Data {
         //Representacao Tabela
         public DbSet<TodoModel>? Todos { get; set; }
 
-        //Configuracao Conexao
+        //Configuracao Conexa
         protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlServer("Server=tcp:dev-proj-server.database.windows.net,1433;Database=devProjDB;User ID=devProjUser;Password=s5D66XF#nO4G");
 
     }
