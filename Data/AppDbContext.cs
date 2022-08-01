0a63e607ce07c6494e5dd9ed61c3398603d1196b
thelrybissi <thelry.d.b@hotmail.com>
2022-08-01 10:25:10 -0300

    Add config conection string to json

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -10,7 +10,9 @@ namespace Todo.Data {
         public DbSet<TodoModel>? Todos { get; set; }
 
         //Configuracao Conexa
-        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlServer("Server=tcp:dev-proj-server.database.windows.net,1433;Database=devProjDB;User ID=devProjUser;Password=s5D66XF#nO4G");
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlServer("Server=localhost,1433;Database=Todo;User ID=sa;Password=1q2w3e4r@#$");
+        
 
     }
 }
